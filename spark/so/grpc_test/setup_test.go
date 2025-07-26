e20cc0f64bedadcc53966d74c6ccb158b4791ec4
Matthew Rheaume <mhr@lightspark.com>
2025-07-25 18:06:49 -0700

    Sync from private repository
    GitOrigin-RevId: 6f6b38f21816366acb86ad763cb39043ed89c3d8

--- spark/so/grpc_test/setup_test.go
+++ spark/so/grpc_test/setup_test.go
@@ -1,20 +1,13 @@
 package grpctest
 
 import (
-	"context"
 	"log/slog"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/btcsuite/btcd/btcjson"
-	"github.com/lightsparkdev/spark/so/dkg"
 	_ "github.com/lightsparkdev/spark/so/ent/runtime"
 	testutil "github.com/lightsparkdev/spark/test_util"
-)
-
-const (
-	EnvRunDKG = "RUN_DKG"
 )
 
 var faucet *testutil.Faucet
@@ -29,17 +22,6 @@ func TestMain(m *testing.M) {
 	}
 
 	faucet = testutil.GetFaucetInstance(client)
-
-	if shouldRunDKG() {
-		if err := setupDKG(); err != nil {
-			slog.Error("DKG setup encountered errors, tests may fail", "error", err)
-		} else {
-			slog.Info("DKG setup completed successfully")
-		}
-	} else {
-		slog.Warn("DKG not run for test setup. Set RUN_DKG=true to run DKG if tests fail, " +
-			"run scripts/run-development-dkg.sh, or re-run tests as they may work on retry")
-	}
 	btcjson.MustRegisterCmd("submitpackage", (*SubmitPackageCmd)(nil), btcjson.UsageFlag(0))
 
 	// Run tests
@@ -49,23 +31,4 @@ func TestMain(m *testing.M) {
 
 	// Teardown
 	os.Exit(code)
-}
-
-func shouldRunDKG() bool {
-	return os.Getenv(EnvRunDKG) == "true"
-}
-
-func setupDKG() error {
-	config, err := testutil.TestConfig()
-	if err != nil {
-		return err
-	}
-
-	if err := dkg.GenerateKeys(context.Background(), config, 1000); err != nil {
-		return err
-	}
-
-	// Allow time for propagation
-	time.Sleep(5 * time.Second)
-	return nil
 }
