509f6093acc79dd55676197dea6db7e20d708dca
Zhen Lu <zhenlu@lightspark.com>
2025-08-28 11:55:38 +0800

    Sync from private repository
    GitOrigin-RevId: 9f5d2869cf72bffaaed95aa32c03970f7813a7c9

--- spark/so/task/task.go
+++ spark/so/task/task.go
@@ -194,6 +194,12 @@ func AllScheduledTasks() []ScheduledTaskSpec {
 							),
 							transfer.And(
 								transfer.StatusEQ(st.TransferStatusSenderKeyTweakPending),
+								transfer.TypeEQ(st.TransferTypeCooperativeExit),
+								transfer.ExpiryTimeLT(time.Now()),
+								transfer.ExpiryTimeNEQ(time.Unix(0, 0)),
+							),
+							transfer.And(
+								transfer.StatusEQ(st.TransferStatusSenderKeyTweakPending),
 								transfer.TypeEQ(st.TransferTypePreimageSwap),
 								transfer.ExpiryTimeLT(time.Now().Add(-24*time.Hour*14)),
 								transfer.ExpiryTimeNEQ(time.Unix(0, 0)),
